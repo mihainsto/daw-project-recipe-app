bfeb7ee978eca5cf4a81efab9d60f21ee97cec6f
mihainsto <mihaistoianro@gmail.com>
2021-01-02 16:40:49 +0200

    Add project

---
+++ proiectDaw/Models/Recipe.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace proiectDaw.Models
+{
+    public class Recipe
+    {
+        [Required]
+        public string Name { get; set; }
+    }
+}
